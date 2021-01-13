c6b167aab3c9bc55ea33d28805eadbfa742feae3
Romano Livi <romano_livi@outlook.com>
2021-01-13 18:38:26 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
-  def change_column(table_name, column_name, type)
+  def change
+    change_column :students, :birthdate, :datetime 
+  end
+end
     
     
